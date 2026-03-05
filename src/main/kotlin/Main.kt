fbefda51f118e80d03ec969cb40a463223a9816b
ccarjim2909 <ccarjim2909@iesrafaelalberti.es>
2026-03-05 09:51:29 +0100

    Ejercicio 1 terminado, falta los permalink

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,39 @@
+import es.ies.ejercicios.u6.ej61.*
+import es.ies.ejercicios.u6.ej62.*
+import es.ies.ejercicios.u6.ej63.*
+import es.ies.ejercicios.u6.ej64.*
+import es.ies.ejercicios.u6.ej65.*
+
+
 fun main() {
     println("Unidad 6 — Ejercicios 6.1 a 6.5")
     println("Implementa cada ejercicio en su paquete y documenta en docs/ejercicios/6.x.md")
+
+
+
+    println("\nUnidad 6 — Ejercicio 6.1")
+
+    val perro = Perro("Lia", "perro", "guau", "Labrador")
+    val zarigueya = Zarigüeya("Zari", "zarigüeya", "chilla")
+    val gato = Gato("Persephone", "miau")
+    val leon = Leon("Simba", "grrr")
+
+    println("\nExtension:\n")
+    println(perro.nombre())
+    println(perro.tipoRaza())
+    println(zarigueya.nombre())
+
+    println()
+
+    println("\nEspecializacion:\n")
+    println(perro.onomatopeya())
+    println(zarigueya.onomatopeya())
+    println(gato.onomatopeya())
+    println(leon.onomatopeya())
+
+
+
+
+
 }
 
